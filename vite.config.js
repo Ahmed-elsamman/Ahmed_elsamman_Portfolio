09a3f26c4a1da2d1097b165634eebcea1d6feb92
Ahmed-elsamman <samman66512@gmail.com>
2024-11-25 17:54:58 +0200

    samman app test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 import compression from 'vite-plugin-compression';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   assetsInclude: ['**/*.pdf'],
@@ -9,6 +10,22 @@ export default defineConfig({
   plugins: [
     svgr(),
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        name: 'Ahmed Elsamman Portfolio',
+        short_name: 'Portfolio',
+        theme_color: '#181a27',
+        icons: [
+          // تحديث الأيقونات حسب manifest.json
+        ],
+      },
+    }),
     compression({
       algorithm: 'gzip',
       ext: '.gz',
@@ -36,12 +53,18 @@ export default defineConfig({
         assetFileNames: 'assets/[name][extname]',
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          utils: ['./src/utils'],
+          components: ['./src/components/'],
+          utils: ['./src/utils/'],
         },
       },
     },
     target: ['es2015', 'edge88', 'firefox78', 'chrome87', 'safari13'],
     polyfillDynamicImport: true,
     minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
   },
 });
